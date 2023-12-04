b777aec126426289047ef72c72ffe368cb184f12
Razalp <razalp0012300@gmail.com>
2023-12-04 13:27:44 +0530

    leaning before react review

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,9 @@ import MainRouter from './ReactRouter/MainRouter'
 function App() {
   const [input,setInput]=useState(0)
   const [count, setCount] = useState(0)
+  const [inputvalue,setInputvalue] =useState('')
   const ref=useRef(0);
-console.log('hellllllo')
+console.log('hellllllo'); 
   function handeler(){
     ref.current = ref.current + 1
     }
@@ -17,17 +18,23 @@ console.log('hellllllo')
       count + 1
     }
 
+    const handleinputchange=(event)=>{
+      setInputvalue(event.target.value)
+    }
 
     console.log(ref)
+     console.log(inputvalue)
   return (
     <>
     <MainRouter></MainRouter>
 
       <div className="card">
-        <input type="text" ref={handeler} onChange={setInput} />
+        <input type="text" ref={handeler} onChange={setCount} />
         <h1>{ref.current}</h1>
          
-        
+        <input type="text" onChange={handleinputchange}/>
+        <h1>this is the changer value {inputvalue}</h1>
+       
        </div>
      
     </>
